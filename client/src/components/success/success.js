e3e9b27a4ee4b42b5dafdeea2dcf256b124162ea
Sarthak Kaushik <skkaushik212@gmail.com>
2023-04-01 10:26:45 +0530

    Success page

--- client/src/components/success/success.js
+++ client/src/components/success/success.js
@@ -1,84 +1,124 @@
-import React, { useState } from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
-import axios from 'axios';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const Success = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [color, setColor] = React.useState("#ffffff");
-    const[productDetails, setProductDetails] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [color, setColor] = React.useState("#ffffff");
+  const [productDetails, setProductDetails] = useState(null);
+  const todayDate = new Date();
+  const date = new Date();
+  date.setDate(date.getDate() + 7);
 
-    const [loading, setLoading] = React.useState(true);
+  const [loading, setLoading] = React.useState(true);
 
-    const name = location?.state?.name ? location?.state?.name: '';
-    const email = location?.state?.email ? location?.state?.email: '';
-    const orderId = location?.state?.order_id ?  location?.state?.order_id: '';
-    const amount = location?.state?.amount ? location?.state?.amount:  '';
-    const productId = location?.state?.productId ? location?.state?.productId: '';
+  const name = location?.state?.name ? location?.state?.name : "";
+  const email = location?.state?.email ? location?.state?.email : "";
+  const orderId = location?.state?.order_id ? location?.state?.order_id : "";
+  const amount = location?.state?.amount ? location?.state?.amount : "";
+  const productId = location?.state?.productId
+    ? location?.state?.productId
+    : "";
 
+  const override = {
+    display: "block",
+    margin: "200 auto",
+    borderColor: "grey",
+  };
 
-    const override = {
-        display: "block",
-        margin: "200 auto",
-        borderColor: "grey",
-      };
-    
-
-    React.useEffect(() => {
-        if(!productId) {
-            navigate('/')
-            window.location.reload()
-        }
-    })
-
-    React.useEffect(async()=> {
-        const productData =  await axios.get(`/api/blog/${productId}`);
-        if(productData && productData.data && productData.data.length) {
-            setProductDetails(productData.data[0])
-        }
-        setLoading(false)
-     },[])
-
-    if(!productDetails) {
-        return (
-            <ClipLoader
-            color={color}
-            loading={loading}
-            cssOverride={override}
-            size={100}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-          )
+  React.useEffect(() => {
+    if (!productId) {
+      navigate("/");
+      window.location.reload();
     }
+  });
 
+  React.useEffect(async () => {
+    const productData = await axios.get(`/api/blog/${productId}`);
+    if (productData && productData.data && productData.data.length) {
+      setProductDetails(productData.data[0]);
+    }
+    setLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!productDetails) {
     return (
-        <div style={{width: '100vw', height: '100vh','padding': '130px 0 90px 0', display:'flex', alignItems:'center', justifyContent: 'center'}}>
-        <div class="bg" style={{width:'60rem'}}>
-            <div class="card checkout-card">
-                <span class="card__success"><i class="ion-checkmark"></i></span>
-                <h1 class="card__msg">Payment Complete</h1>
-                <h2 class="card__submsg">Thank you for your transfer</h2>
-                <h2>We will shortly send you the email for your order</h2>
-                <div class="card__body">
-                {productDetails?  <img src={`./img/${productDetails?.meta_description}/${productDetails?.front_image}`}class="card__avatar" /> : ''}
-                <div class="card__recipient-info">
-                    <p class="card__recipient">{name}</p>
-                    <p class="card__email">{email}</p>
-                </div>
-                
-                <h1 class="card__price"><span>₹</span>{amount}<span>.00</span></h1>
-                </div>
-                <div class="card__tags">
-                    <span class="card__tag">completed</span>
-                    <span class="card__tag">#{orderId}</span>
-                </div>
-            </div>
-        </div>
-        </div>
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    );
+  }
 
-    )
-}
+  //   <img
+  //   src={`./img/${productDetails?.meta_description}/${productDetails?.front_image}`}
+  //   class="card__avatar"
+  // />
+
+  return (
+    <div class="container mt-5 padding-top-50 font-family-poppins">
+      <div class="d-flex align-items-center justify-content-between flex-change">
+        <div class="mt-4">
+          <h1 class="font-family-poppins" style={{ fontWeight: "700" }}>
+            Payment Successfull
+          </h1>
+          <p class="font-family-poppins">
+            Thank you for your transfer. We will shortly send you the email for
+            your order
+          </p>
+
+          <div class="row">
+            <div class="col-12 col-md-10 hh-grayBox">
+              <div class="row justify-content-between">
+                <div class="order-tracking completed">
+                  <span class="is-complete"></span>
+                  <p>
+                    Ordered
+                    <br />
+                    <span>{todayDate.toDateString()}</span>
+                  </p>
+                </div>
+                <div class="order-tracking completed">
+                  <span class="is-complete"></span>
+                  <p>
+                    Confirmed
+                    <br />
+                    <span>{todayDate.toDateString()}</span>
+                  </p>
+                </div>
+                <div class="order-tracking">
+                  <span class="is-complete"></span>
+                  <p>
+                    Delivered
+                    <br />
+                    <span>{date.toDateString()}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <a href="/">
+            {" "}
+            <button className="btn btn-primary">Continue Shopping </button>
+          </a>
+
+          {/* <div className="succssdetails"></div> */}
+        </div>
+        <div className="mt-4">
+          <img src="./img/success.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Success;
